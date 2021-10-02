5b6013637d0086ae9517e63508aeef4eddbb9f61
Jaeger2305 <rwebb2305@gmail.com>
2021-10-02 20:29:55 +0200

    feat: enemy ai with shooting

--- Assets/EnemyAI.cs
+++ Assets/EnemyAI.cs
@@ -26,17 +26,26 @@ public class EnemyAI : MonoBehaviour
         GoingBackToStart,
     }
 
-    //private EnemyPathfindingMovement pathfindingMovement;
     public AIPath aiPath;
     private Vector3 startingPosition;
-    private Vector3 roamPosition;
     private float nextShootTime;
     private State state;
     private GameObject player;
+    private AIDestinationSetter aiDestinationSetter;
+    private GameObject fabricatedDestination;
+    [SerializeField]
+    private Vector2 patrolRange = new Vector2(10f, 6f);
+    [SerializeField]
+    private float visionRange = 12f;
+    [SerializeField]
+    private float stopChaseDistance = 15f;
+    [SerializeField]
+    private float attackRange = 10f;
 
     private void Awake()
     {
-        //pathfindingMovement = GetComponent<EnemyPathfindingMovement>();
+        aiPath = GetComponent<AIPath>();
+        aiDestinationSetter = GetComponent<AIDestinationSetter>();
         state = State.Roaming;
         player = GameObject.FindGameObjectWithTag("Player");
     }
@@ -44,76 +53,68 @@ public class EnemyAI : MonoBehaviour
     private void Start()
     {
         startingPosition = transform.position;
-        roamPosition = GetRoamingPosition();
+        fabricatedDestination = new GameObject();
+        fabricatedDestination.transform.position = GetRoamingPosition();
+        aiDestinationSetter.target = fabricatedDestination.transform;
     }
 
     private void Update()
     {
-        //switch (state)
-        //{
-        //    default:
-        //    case State.Roaming:
-        //        pathfindingMovement.MoveToTimer(roamPosition);
+        switch (state)
+        {
+            default:
+            case State.Roaming:
+                if (aiPath.reachedDestination)
+                {
+                    Debug.Log("Reached patrol path, getting new patrol spot");
+                    fabricatedDestination.transform.position = GetRoamingPosition();
+                }
 
-        //        float reachedPositionDistance = 10f;
-        //        if (Vector3.Distance(transform.position, roamPosition) < reachedPositionDistance)
-        //        {
-        //            // Reached Roam Position
-        //            roamPosition = GetRoamingPosition();
-        //        }
+                FindTarget();
+                break;
+            case State.ChaseTarget:
+                if (Vector3.Distance(transform.position, player.transform.position) > stopChaseDistance)
+                {
+                    Debug.Log("Player out of chase range, going back to start");
+                    state = State.GoingBackToStart;
+                }
 
-        //        FindTarget();
-        //        break;
-        //    case State.ChaseTarget:
-        //        pathfindingMovement.MoveToTimer(player.transform.position);
-
-        //        float attackRange = 30f;
-        //        if (Vector3.Distance(transform.position, player.transform.position) < attackRange)
-        //        {
-        //            // Target within attack range
-        //            if (Time.time > nextShootTime)
-        //            {
-        //                pathfindingMovement.StopMoving();
-        //                state = State.ShootingTarget;
-        //                float fireRate = .15f;
-        //                nextShootTime = Time.time + fireRate;
-        //            }
-        //        }
-
-        //        float stopChaseDistance = 80f;
-        //        if (Vector3.Distance(transform.position, player.transform.position) > stopChaseDistance)
-        //        {
-        //            // Too far, stop chasing
-        //            state = State.GoingBackToStart;
-        //        }
-        //        break;
-        //    case State.ShootingTarget:
-        //        break;
-        //    case State.GoingBackToStart:
-        //        pathfindingMovement.MoveToTimer(startingPosition);
-
-        //        reachedPositionDistance = 10f;
-        //        if (Vector3.Distance(transform.position, startingPosition) < reachedPositionDistance)
-        //        {
-        //            // Reached Start Position
-        //            state = State.Roaming;
-        //        }
-        //        break;
-        //}
+                if (Vector3.Distance(transform.position, player.transform.position) < attackRange)
+                {
+                    state = State.ShootingTarget;
+                }
+                break;
+            case State.ShootingTarget:
+                if (Vector3.Distance(transform.position, player.transform.position) < attackRange)
+                {
+                    Debug.Log("Player in range, firing");
+                    GetComponent<ProjectileController>().FireAtPlayer();
+                } else
+                {
+                    state = State.ChaseTarget;
+                }
+                break;
+            case State.GoingBackToStart:
+                fabricatedDestination.transform.position = startingPosition;
+                aiDestinationSetter.target = fabricatedDestination.transform;
+                state = State.Roaming;
+                break;
+        }
     }
 
     private Vector3 GetRoamingPosition()
     {
-        return startingPosition * Random.Range(10f, 70f);
+        return startingPosition + new Vector3(Random.Range(-patrolRange.x, patrolRange.x), Random.Range(-patrolRange.y, patrolRange.y), 1);
     }
 
     private void FindTarget()
     {
-        float targetRange = 50f;
-        if (Vector3.Distance(transform.position, player.transform.position) < targetRange)
+        if (Vector3.Distance(transform.position, player.transform.position) < visionRange)
         {
+            Debug.Log("Found player target");
             // Player within target range
             state = State.ChaseTarget;
+            aiDestinationSetter.target = player.transform;
         }
     }
 
--- Assets/ProjectileController.cs
+++ Assets/ProjectileController.cs
@@ -5,16 +5,21 @@ using UnityEngine;
 public class ProjectileController : MonoBehaviour
 {
     public GameObject projectilePrefab;
-    private int shotsPerMinute = 30;
+    private float cooldownBetweenShots = 0.5f;
+    private float cooldown = 0f;
 
-    private void Start()
+    private void Update()
     {
-        InvokeRepeating("FireAtPlayer", 1f, 60 / shotsPerMinute);
+        cooldown = Mathf.Clamp(cooldown - Time.deltaTime, 0f, cooldownBetweenShots);
     }
     public void FireAtPlayer()
     {
-        GameObject player = GameObject.FindGameObjectWithTag("Player");
-        Projectile projectile = GameObject.Instantiate(projectilePrefab).GetComponent<Projectile>();
-        projectile.Init(player.transform, transform, 20, 5f);
+        if (cooldown <= 0)
+        {
+            cooldown += cooldownBetweenShots;
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
+            Projectile projectile = Instantiate(projectilePrefab).GetComponent<Projectile>();
+            projectile.Init(player.transform, transform, 20, 5f);
+        }
     }
 }
